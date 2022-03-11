6e3103d9cd880bcd42f90d3cc89b39bfab74f04b
Anett88 <sz.anett888@gmail.com>
2022-03-11 14:29:16 +0100

    elso comitalas

---
+++ src/konflikuskezeles/Konflikuskezeles.java
@@ -0,0 +1,15 @@
+
+package konflikuskezeles;
+
+
+public class Konflikuskezeles {
+
+   
+    public static void main(String[] args) {
+        String nev1;
+        
+        
+        
+    }
+    
+}
